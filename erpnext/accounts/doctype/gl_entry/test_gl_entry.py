6522c6b9ee235b4c033060212b1b5ad9b844e17b
priyanshshah2442 <priyanshshah2442@gmail.com>
2024-07-05 16:25:27 +0530

    fix: test case for validate balance type

--- erpnext/accounts/doctype/gl_entry/test_gl_entry.py
+++ erpnext/accounts/doctype/gl_entry/test_gl_entry.py
@@ -10,15 +10,10 @@ from frappe.model.naming import parse_naming_series
 from erpnext.accounts.doctype.gl_entry.gl_entry import rename_gle_sle_docs
 from erpnext.accounts.doctype.journal_entry.test_journal_entry import make_journal_entry
 
-test_dependencies = ["Company", "Account"]
+test_dependencies = ["Company", "Account", "Finance Book"]
 
 
 class TestGLEntry(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		super().setUpClass()
-		create_finance_books()
-
 	def test_round_off_entry(self):
 		frappe.db.set_value("Company", "_Test Company", "round_off_account", "_Test Write Off - _TC")
 		frappe.db.set_value("Company", "_Test Company", "round_off_cost_center", "_Test Cost Center - _TC")
@@ -88,36 +83,55 @@ class TestGLEntry(unittest.TestCase):
 		self.assertEqual(old_naming_series_current_value + 2, new_naming_series_current_value)
 
 	def test_validate_balance_type(self):
-		frappe.db.set_value("Account", "_Test Fixed Asset - _TC", "balance_must_be", "Debit")
+		account1_name = "_Test Finance Books"
+		account2_name = "_Test Bank"
+		account1 = account1_name + " - _TC"
+		account2 = account2_name + " - _TC"
 
-		je = make_journal_entry("_Test Fixed Asset - _TC", "_Test Bank - _TC", 1000, save=False)
-		je.finance_book = "Financial Accounting"
-		je.insert()
-		je.submit()
+		if not frappe.db.exists("Account", {"account_name": account1_name, "company": "_Test Company"}):
+			create_test_account(account1_name)
+		else:
+			clear_previous_balances(account1)
 
-		financial_accounting_je = make_journal_entry(
-			"_Test Fixed Asset - _TC", "_Test Bank - _TC", -500, save=False
-		)
+		make_journal_entry(account1, account2, 1000, submit=True)
+
+		financial_accounting_je = make_journal_entry(account1, account2, -1000, save=False)
 		financial_accounting_je.finance_book = "Financial Accounting"
 		financial_accounting_je.insert()
 		financial_accounting_je.submit()
 
-		tax_accounting_je = make_journal_entry(
-			"_Test Fixed Asset - _TC", "_Test Bank - _TC", -500, save=False
-		)
-		tax_accounting_je.finance_book = "Tax Accounting"
-		tax_accounting_je.insert()
+		tax_accounting_je1 = make_journal_entry(account1, account2, -500, save=False)
+		tax_accounting_je1.finance_book = "Tax Accounting"
+		tax_accounting_je1.insert()
+		tax_accounting_je1.submit()
+
+		tax_accounting_je2 = make_journal_entry(account1, account2, -600, save=False)
+		tax_accounting_je2.finance_book = "Tax Accounting"
+		tax_accounting_je2.insert()
 
 		self.assertRaisesRegex(
 			frappe.ValidationError,
-			re.compile(r"^(Balance for Account.*must always be Debit)"),
-			tax_accounting_je.submit,
+			re.compile(r"^(Balance for Account .* must always be Debit)"),
+			tax_accounting_je2.submit,
 		)
 
 
-def create_finance_books():
-	for finance_book in ["Financial Accounting", "Tax Accounting"]:
-		if not frappe.db.exists("Finance Book", finance_book):
-			fb = frappe.new_doc("Finance Book")
-			fb.finance_book_name = finance_book
-			fb.insert()
+def create_test_account(account_name):
+	account = frappe.new_doc("Account")
+	account.update(
+		{
+			"account_name": account_name,
+			"parent_account": "Current Assets",
+			"company": "_Test Company",
+			"is_group": 0,
+			"account_type": "Fixed Asset",
+			"account_currency": "INR",
+			"balance_must_be": "Debit",
+		}
+	)
+	account.insert()
+
+
+def clear_previous_balances(account_name):
+	gl = frappe.qb.DocType("GL Entry")
+	frappe.qb.from_(gl).delete().where(gl.account == account_name).run()
